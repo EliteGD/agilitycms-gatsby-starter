77612d4919d863e5331a6834986ebe079d22fa42
Mohammed Arefeen <mohammed.arefeen@mnp.ca>
2021-11-22 16:08:24 -0500

    devops

--- src/components/common/Map/Map.jsx
+++ src/components/common/Map/Map.jsx
@@ -13,7 +13,7 @@ const Map = ({ markers, center: currCenter }) => {
     zoom: 11,
   }
 
-  console.log('some api test',process.env.TEST_API)
+  console.log('some api test',process.env.GATSBY_TEST_API)
 
   return (
     <div style={{ height: "600px", width: "100%" }}>
